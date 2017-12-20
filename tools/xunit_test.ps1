548a57165e47b525c9091b3df1fe08650dc431ca
Roman Bolshakov <rombolshak@yandex.ru>
2017-12-20 15:55:12 +0300

    logging test assemblies

--- tools/xunit_test.ps1
+++ tools/xunit_test.ps1
@@ -1,3 +1,4 @@
 $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
 $tests = gci -Path "$sciptDirectory\..\bin" -Filter "*Tests.dll" -Recurse |% { $_.FullName }
+Write-Host These assemblies contains tests: $tests
 & "$env:xunit20\xunit.console" -xml $sciptDirectory\..\TestResult.xml $tests
