472fee1aa6f5fa9d27e616d04ab40d256953a061
Roman Bolshakov <rombolshak@yandex.ru>
2017-12-20 15:50:09 +0300

    вжух

--- tools/xunit_test.ps1
+++ tools/xunit_test.ps1
@@ -1,3 +1,3 @@
 $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
 $tests = gci -Path "$sciptDirectory\..\bin" -Filter "*Tests.dll" -Recurse |% { $_.FullName }
-& "$env:xunit20\xunit.console" $tests -xml=$sciptDirectory\..\TestResult.xml
+& "$env:xunit20\xunit.console" -xml $sciptDirectory\..\TestResult.xml $tests
