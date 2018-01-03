b60f22c03325c7735fb48b470abc9cda27975397
Roman Bolshakov <rombolshak@yandex.ru>
2018-01-03 21:28:52 +0300

    autofac usage

---
+++ src/RombSkeleton.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using RombSkeleton.Containers;
+
+namespace RombSkeleton.App
+{
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Application startup")]
+	internal class Program
+	{
+		// ReSharper disable once UnusedMember.Local
+		private static void Main()
+		{
+			using (var container = new IocContainer())
+			{
+				var test = new TestContainer(container);
+				container.OnRegistrationComplete();
+
+				Console.WriteLine(test.TestClass().Add(4, 3));
+				Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ src/RombSkeleton.Containers/IocContainer.cs
@@ -0,0 +1,55 @@
+﻿#region Usings
+
+using System;
+using Autofac;
+
+#endregion
+
+namespace RombSkeleton.Containers
+{
+	public sealed class IocContainer : IDisposable
+	{
+		public IocContainer()
+		{
+			_builder = new ContainerBuilder();
+		}
+
+		public Func<T> Register<T>(Func<T> objectCreator)
+		{
+			if (_scope != null)
+			{
+				throw new InvalidOperationException($"Cannot register {typeof(T)}: container registration is complete");
+			}
+
+			_builder.Register(_ => objectCreator());
+			return GetObjectGetter<T>();
+		}
+
+		public void OnRegistrationComplete()
+		{
+			_container = _builder.Build();
+			_scope = _container.BeginLifetimeScope();
+		}
+
+		public void Dispose()
+		{
+			_scope?.Dispose();
+			_container?.Dispose();
+		}
+
+		private Func<T> GetObjectGetter<T>() =>
+			() =>
+			{
+				if (_scope == null)
+				{
+					throw new InvalidOperationException($"Cannot resolve {typeof(T)} as objects registration is not complete yet");
+				}
+
+				return _scope.Resolve<T>();
+			};
+
+		private readonly ContainerBuilder _builder;
+		private IContainer _container;
+		private ILifetimeScope _scope;
+	}
+}
